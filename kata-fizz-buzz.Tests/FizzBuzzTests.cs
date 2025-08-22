68d6540de63d78be327628caf65e76b718e92f61
Yousra <yousra.laribi@gmail.com>
2025-08-22 10:06:40 +0200

    init commit - add first method to return fizz, buzz and fizzbuzz

---
+++ kata-fizz-buzz.Tests/FizzBuzzTests.cs
@@ -0,0 +1,25 @@
+﻿namespace kata_fizz_buzz.Tests
+{
+    public class FizzBuzzTests
+    {
+        [Fact]
+        public void Should_Throw_Exception_When_Input_Is_Less_Than_1()
+        {
+            var process = new FizzBuzz();
+            Assert.Throws<ArgumentOutOfRangeException>(() => process.Do(0));
+            Assert.Throws<ArgumentOutOfRangeException>(() => process.Do(-1));
+        }
+
+        [Theory]
+        [InlineData(1, "1")]
+        [InlineData(3, "Fizz")]
+        [InlineData(5, "Buzz")]
+        [InlineData(15, "FizzBuzz")]
+        public void Should_Return_Correct_String(int input, string output)
+        {
+            var process = new FizzBuzz();
+            var response = process.Do(input);
+            Assert.Equal(output, response);
+        }
+    }
+}
---
+++ kata-fizz-buzz/FizzBuzz.cs
@@ -0,0 +1,26 @@
+﻿namespace kata_fizz_buzz
+{
+    public class FizzBuzz
+    {
+        private const string FIZZ = "Fizz";
+        private const string BUZZ = "Buzz";
+        public string Do(int input)
+        {
+            if (input < 1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(input), "Input must be a positive integer.");
+            }
+            var response = string.Empty;
+            if (input % 3 == 0)
+            {
+                response = FIZZ;
+            }
+            if (input % 5 == 0)
+            {
+                response = $"{response}{BUZZ}";
+            }
+            return string.IsNullOrEmpty(response) ? input.ToString() : response;
+        }
+
+    }
+}
